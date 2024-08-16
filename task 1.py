ad7cf9fd21c183394569b470f9da398de7dd1f19
rezasheikhloo <m.rezasheikhloo@gmail.com>
2024-08-16 19:48:41 +0330

    5/26

--- task 1.py
+++ task 1.py
@@ -1,28 +1,44 @@
 import tkinter
 import tkinter.ttk as ttk
-def test():
-    print(brand.get())
-    print(Model.get())
 
-def reset_form():
-    brand.set("")
-    Model.set("")
-    color.set("")
-    glass.set(False)
-    Memory.set(False)
+def test():
+    print(model.get())
 
 win = tkinter.Tk()
-win.title("Mobile info")
-win.geometry("600x300")
 
-tkinter.Label(win, text="brand").place(x=20, y=40)
+win.title("mobile Info")
+win.geometry("300x300")
+
+tkinter.Label(win, text="brand").place(x=20, y=20)
 brand = tkinter.StringVar()
 ttk.Combobox(win,
-textvariable=brand,
-values=["Apple", "Samsung", "Sony", "Nokia"],
-state="readonly"
-).place(x=20, y=60)
+             textvariable=brand,
+             values=["apple", "samsung", "sony", "nokia"],
+             state="readonly"
+             ).place(x=80, y=20)
 
-tkinter.Label(win, text="Model").place(x=20, y=60)
-Model = tkinter.StringVar()
-tkinter.Entry(win, textvariable=Model,width=23).place(x=40, y=60)
+tkinter.Label(win, text="model").place(x=20, y=60)
+model = tkinter.StringVar()
+tkinter.Entry(win, textvariable=model).place(x=80, y=60)
+
+tkinter.Label(win, text="color").place(x=20, y=100)
+color = tkinter.StringVar()
+ttk.Combobox(win,
+             textvariable=color,
+             values=["rose_gold", "white", "silver", "black , others"],
+             state="readonly"
+             ).place(x=80, y=100)
+
+
+tkinter.Label(win, text="glass").place(x=20, y=140)
+glass = tkinter.BooleanVar()
+tkinter.Checkbutton(win, text="glass", variable=glass).place(x=80, y=140)
+
+tkinter.Label(win, text="memory").place(x=20, y=180)
+memory = tkinter.BooleanVar()
+tkinter.Checkbutton(win, text="memory", variable=memory).place(x=80, y=180)
+
+tkinter.Button(win, text="Sell", width=10, command=test).place(x=80,y=240)
+
+win.mainloop()
+
